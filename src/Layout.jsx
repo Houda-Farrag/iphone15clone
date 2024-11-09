050295a8574da543451122446c6909d7fbfc9fd9
Houda-Farrag <mahmiti2022@gmail.com>
2024-11-10 00:24:35 +0200

    finish the highlits section

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import {Outlet} from 'react-router-dom'
+import React from "react";
+import { Outlet } from "react-router-dom";
 import Header from "./components/Header/Header.jsx";
 
 const AppLayOut = () => {
-    return <>
-        <main className='bg-black'>
-           <Header/> 
-            <Outlet/>
-       </main>
+  return (
+    <>
+      <main className="bg-black">
+        <Header />
+        <Outlet />
+      </main>
+    </>
+  );
+};
 
-
-   </>
-}
-
-export default AppLayOut
+export default AppLayOut;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from 'react'
 import Heros from "../../components/Heros/Heros.jsx";
 import Highlights from '../../components/Highlights/Highlights.jsx';
+import ModelPhone from '../../components/ModelPhone/ModelPhone.jsx';
 
-export  function Home() {
- 
- return <>
-  <Heros/>
-<Highlights/>
- </>
-}
+export function Home() {
+
+  return <>
+    <Heros />
+    <Highlights />
+    <ModelPhone/>
+  </>
+} 
 
 export default Home
